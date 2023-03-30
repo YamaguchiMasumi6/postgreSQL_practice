1995cf3c3f8767e5974b8153458b7bd5ca90472e
YamaguchiMasumi6 <yoikaze0619@gmail.com>
2023-03-30 21:43:45 +0900

    ログイン処理完了

---
+++ SampleWebApp/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,63 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Login;
+import model.LoginLogic;
+
+/**
+ * Servlet implementation class LoginServlet
+ *  TODO #104 ログイン　ログインに関するリクエストを処理するコントローラー
+ *  （遷移のみ）
+ */
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+    public LoginServlet() {
+        super();
+    }
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// フォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8"); // 文字化け防止
+		String userId = request.getParameter("userId");
+		String pass = request.getParameter("pass");
+		
+		// ログイン処理の実行
+		Login login = new Login(userId, pass);
+		LoginLogic bo = new LoginLogic();
+		boolean result = bo.excute(login);
+		
+		// ログイン処理の成否によって処理を分岐
+		if (result) { // ログイン成功時
+			// セッションスコープにユーザーIDを保存
+			HttpSession session = request.getSession(); // セッションオブジェクトの取得
+			session.setAttribute("userId", userId); // セッションに保存
+			
+			// ログイン結果画面にフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/loginOK.jsp");
+			dispatcher.forward(request, response);
+		} else { // ログイン失敗時
+			// リダイレクト
+			response.sendRedirect("Login");
+		}
+		
+		
+	}
+
+}
---
+++ SampleWebApp/src/main/webapp/WEB-INF/jsp/loginOK.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>スッキリ商店</title>
+</head>
+<body>
+	<p>ようこそ さん</p>
+	<a href="Welcome">トップへ</a>
+	<a href="">一覧画面へ</a>
+</body>
+</html>
